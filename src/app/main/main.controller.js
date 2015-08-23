b1431a5d522ee4dfc86fbfe06a519c6896609cb2
Benjamin Murden <ben.murden@gmail.com>
2015-08-23 20:53:34 +0900

    Add keyUp handling, keySequence.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,7 +6,7 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webAudioPlayer, webDevTec, toastr) {
+  function MainController($timeout, $log, webAudioPlayer, webDevTec, toastr) {
     var vm = this;
 
     vm.awesomeThings = [];
@@ -19,9 +19,25 @@
       s: 0.5,
       r: 0.25
     };
+    vm.keySequence = [90,88,67,86,66,78,77,188,190,65,83,68,70,71,72,74,75,76,186,222];
+    vm.keysDown = [];
 
     vm.keyDown = function(e) {
-      console.log(e);
+      var index = vm.keysDown.indexOf(e.keyCode);
+
+      if (index === -1) {
+        vm.keysDown.push(e.keyCode);
+        $log.log(e.keyCode);
+      }
+    };
+
+    vm.keyUp = function(e) {
+      var index = vm.keysDown.indexOf(e.keyCode);
+
+      if (index !== -1) {
+        vm.keysDown.pop(index);
+        $log.log('Key up: ' + e.keyCode);
+      }
     };
 
     activate();
