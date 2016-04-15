e5f2407f5642359f17921fa86838b44a7feb5228
Benjamin Murden <ben.murden@gmail.com>
2016-04-15 21:22:00 +0900

    Key mapping update.
    Improve piano key mapping by using webAudio notes array and key pattern
    in a combined loop.

--- src/app/components/tn-keyboard/tn-keyboard.directive.js
+++ src/app/components/tn-keyboard/tn-keyboard.directive.js
@@ -16,17 +16,37 @@
       bindToController: {
           notesPlaying: '=',
           scale: '=',
-          baseKeyOffset: '='
+          baseKeyOffset: '=',
+          baseOctave: '='
       }
     };
 
     return directive;
 
     /** @ngInject */
-    function TnKeyboardController() {
+    function TnKeyboardController($scope, $log, webAudioPlayer) {
       var vm = this;
 
       vm.keyPattern = [0,1,0,1,0,0,1,0,1,0,1,0];
+      vm.keyboardNotes = [];
+
+      vm.generateKeyboardNotes = function() {
+        for (var i = vm.baseOctave * 12; i < (vm.baseOctave + 2) * 12; i++) {
+          vm.keyboardNotes.push({
+            label: webAudioPlayer.noteList[i],
+            type: vm.keyPattern[i % 12]
+          });
+        }
+      };
+
+      activate();
+
+      function activate() {
+        vm.generateKeyboardNotes();
+        $log.log(vm.keyboardNotes);
+      }
+
+      return vm;
     }
   }
 
