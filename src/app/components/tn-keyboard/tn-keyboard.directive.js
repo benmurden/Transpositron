5d21dc23c3044b983c74116883b4e2e9dfec3d8e
Benjamin Murden <ben.murden@gmail.com>
2016-04-22 11:24:05 +0900

    Add multi-touch keyboard support.

--- src/app/components/tn-keyboard/tn-keyboard.directive.js
+++ src/app/components/tn-keyboard/tn-keyboard.directive.js
@@ -33,11 +33,13 @@
       vm.keyboardNotes = [];
 
       vm.generateKeyboardNotes = function() {
+        var j = 0;
         for (var i = vm.baseOctave * 12; i < (vm.baseOctave + 3) * 12; i++) {
-          vm.keyboardNotes.push({
+          vm.keyboardNotes[j] = {
             label: webAudioPlayer.noteList[i],
             type: vm.keyPattern[i % 12]
-          });
+          };
+          j++;
         }
       };
 
@@ -53,12 +55,71 @@
         vm.stopNote()(note);
       };
 
+      vm.notesDown = function(notes) {
+        notes.forEach(function(v) {
+          vm.playNote()(v);
+        });
+      };
+
+      vm.notesUp = function(notes) {
+        notes.forEach(function(v) {
+          vm.stopNote()(v);
+        });
+      };
+
       vm.mouseOver = function(e, note) {
         // Send noteDown if only the left mouse button is held.
         if (e.buttons === 1) {
           return vm.noteDown(note);
         }
       };
+
+      vm.touchmove = function(e) {
+        vm.touchesToNotes(e.touches);
+      };
+
+      vm.touchStart = function(e) {
+        vm.touchesToNotes(e.touches);
+      };
+
+      vm.touchEnd = function(e) {
+        vm.touchesToNotes(e.touches);
+      };
+
+      vm.touchesToNotes = function(touches) {
+        var touchNotes = [];
+        _.forEach(touches, function(v) {
+          var targetElement = document.elementFromPoint(v.clientX, v.clientY);
+          if (targetElement !== null) {
+            touchNotes.push({key: targetElement.id});
+          }
+        });
+
+        vm.syncNotesPlaying(touchNotes);
+
+        vm.notesPlaying = touchNotes;
+      };
+
+      vm.syncNotesPlaying = function(notes) {
+        var partitionedNotes = _.partition(notes, function(v) {
+          return _.some(vm.notesPlaying, v);
+        });
+        var keyMap = function(v) {
+          return v.key;
+        };
+        var flatNotesPlaying = _.map(vm.notesPlaying, keyMap);
+        var flatTouchNotes = _.map(notes, keyMap);
+
+        var newNotes = _.difference(flatTouchNotes, flatNotesPlaying);
+        var oldNotes = _.difference(flatNotesPlaying, flatTouchNotes);
+
+        $log.log(newNotes, oldNotes);
+
+        vm.notesDown(newNotes);
+        vm.notesUp(oldNotes);
+      };
+
+      $scope.$watch('vm.baseOctave', vm.generateKeyboardNotes);
 
       activate();
 
