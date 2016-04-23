cb9d71344250d8f1d8f4f1b5b819c0ae0587a4fc
Benjamin Murden <ben.murden@gmail.com>
2016-04-23 17:07:23 +0900

    Update 2016-04-23T08:07:18.332Z

--- scripts/app.js
+++ scripts/app.js
@@ -522,91 +522,6 @@
 
   angular
     .module('transpositron')
-    .directive('tnTouchmove', tnTouchMove)
-    .directive('tnTouchstart', tnTouchStart)
-    .directive('tnTouchend', tnTouchEnd);
-
-  /** @ngInject */
-  function tnTouchMove() {
-    var directive = {
-      restrict: 'A',
-      controller: TnTouchMoveController
-    };
-
-    return directive;
-
-    function TnTouchMoveController($scope, $element) {
-      $element.bind("touchstart", onTouchStart);
-
-      function onTouchStart(event) {
-        event.preventDefault();
-        $element.bind("touchmove", onTouchMove);
-        $element.bind("touchend", onTouchEnd);
-      }
-
-      function onTouchMove(event) {
-        var method = $element.attr("tn-touchmove");
-        $scope.$event = event;
-        $scope.$apply(method);
-      }
-
-      function onTouchEnd(event) {
-        event.preventDefault();
-        $element.unbind("touchmove", onTouchMove);
-        $element.unbind("touchend", onTouchEnd);
-      }
-
-    }
-  }
-
-  /** @ngInject */
-  function tnTouchStart() {
-    var directive = {
-      restrict: 'A',
-      controller: TnTouchStartController
-    };
-
-    return directive;
-
-    function TnTouchStartController($scope, $element) {
-      $element.bind('touchstart', onTouchStart);
-
-      function onTouchStart(event) {
-        event.preventDefault();
-        var method = $element.attr('tn-touchstart');
-        $scope.$event = event;
-        $scope.$apply(method);
-      }
-    }
-  }
-
-  /** @ngInject */
-  function tnTouchEnd() {
-    var directive = {
-      restrict: 'A',
-      controller: TnTouchEndController
-    };
-
-    return directive;
-
-    function TnTouchEndController($scope, $element) {
-      $element.bind('touchend', onTouchEnd);
-
-      function onTouchEnd(event) {
-        event.preventDefault();
-        var method = $element.attr('tn-touchend');
-        $scope.$event = event;
-        $scope.$apply(method);
-      }
-    }
-  }
-})();
-
-(function() {
-  'use strict';
-
-  angular
-    .module('transpositron')
     .directive('tnKeyboard', tnKeyboard);
 
   /** @ngInject */
@@ -745,6 +660,91 @@
     }
   }
 
+})();
+
+(function() {
+  'use strict';
+
+  angular
+    .module('transpositron')
+    .directive('tnTouchmove', tnTouchMove)
+    .directive('tnTouchstart', tnTouchStart)
+    .directive('tnTouchend', tnTouchEnd);
+
+  /** @ngInject */
+  function tnTouchMove() {
+    var directive = {
+      restrict: 'A',
+      controller: TnTouchMoveController
+    };
+
+    return directive;
+
+    function TnTouchMoveController($scope, $element) {
+      $element.bind("touchstart", onTouchStart);
+
+      function onTouchStart(event) {
+        event.preventDefault();
+        $element.bind("touchmove", onTouchMove);
+        $element.bind("touchend", onTouchEnd);
+      }
+
+      function onTouchMove(event) {
+        var method = $element.attr("tn-touchmove");
+        $scope.$event = event;
+        $scope.$apply(method);
+      }
+
+      function onTouchEnd(event) {
+        event.preventDefault();
+        $element.unbind("touchmove", onTouchMove);
+        $element.unbind("touchend", onTouchEnd);
+      }
+
+    }
+  }
+
+  /** @ngInject */
+  function tnTouchStart() {
+    var directive = {
+      restrict: 'A',
+      controller: TnTouchStartController
+    };
+
+    return directive;
+
+    function TnTouchStartController($scope, $element) {
+      $element.bind('touchstart', onTouchStart);
+
+      function onTouchStart(event) {
+        event.preventDefault();
+        var method = $element.attr('tn-touchstart');
+        $scope.$event = event;
+        $scope.$apply(method);
+      }
+    }
+  }
+
+  /** @ngInject */
+  function tnTouchEnd() {
+    var directive = {
+      restrict: 'A',
+      controller: TnTouchEndController
+    };
+
+    return directive;
+
+    function TnTouchEndController($scope, $element) {
+      $element.bind('touchend', onTouchEnd);
+
+      function onTouchEnd(event) {
+        event.preventDefault();
+        var method = $element.attr('tn-touchend');
+        $scope.$event = event;
+        $scope.$apply(method);
+      }
+    }
+  }
 })();
 
 (function() {
