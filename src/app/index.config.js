2fedd669f57e403f7008d3bb1b0ffd7aed3ebb60
Benjamin Murden <ben.murden@gmail.com>
2016-04-28 21:41:09 +0900

    Add ngRoute and define routes.

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -6,7 +6,7 @@
     .config(config);
 
   /** @ngInject */
-  function config($logProvider, toastr, $mdThemingProvider) {
+  function config($logProvider, toastr, $mdThemingProvider, $routeProvider, $locationProvider) {
     // Enable log
     $logProvider.debugEnabled(true);
 
@@ -20,6 +20,18 @@
       .primaryPalette('orange')
       .accentPalette('deep-orange')
       .dark();
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/home/home.html',
+        controller: 'HomeController',
+        controllerAs: 'home'
+      })
+      .when('/about/', {
+        templateUrl: 'app/about/about.html'
+      });
+
+    $locationProvider.html5Mode(true);
 
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
   }
