ef87b66eb783ca6c11538346ca57d897903a7ace
Benjamin Murden <ben.murden@gmail.com>
2016-04-28 21:42:33 +0900

    Add base tag dynamically.

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,7 @@
   'use strict';
 
   angular
-    .module('transpositron', ['ngAnimate', 'ngCookies', 'ngMaterial']);
+    .module('transpositron', ['ngAnimate', 'ngCookies', 'ngMaterial', 'ngRoute']);
 
+    angular.element(document.getElementsByTagName('head')).append(angular.element('<base href="' + window.location.pathname + '" />'));
 })();
