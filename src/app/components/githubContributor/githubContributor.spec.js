ebbe837d06352df2aad46262a2e4df80a895d367
Benjamin Murden <ben.murden@gmail.com>
2016-05-21 21:21:36 +0900

    Complete githubContributors coverage

--- src/app/components/githubContributor/githubContributor.spec.js
+++ src/app/components/githubContributor/githubContributor.spec.js
@@ -2,14 +2,22 @@
   'use strict';
 
   describe('GitHub contributor factory', function(){
-    var factory, $httpBackend;
+    var factory, $httpBackend, $log, apiUrl;
+
+    apiUrl = 'https://api.github.com/repos/benmurden/transpositron/contributors?per_page=';
 
     beforeEach(module('transpositron'));
 
-    beforeEach(inject(function(githubContributor, _$httpBackend_) {
+    beforeEach(inject(function(githubContributor, _$httpBackend_, _$log_) {
         factory = githubContributor;
         $httpBackend = _$httpBackend_;
+        $log = _$log_;
     }));
+
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
 
     it('is available', inject(function(){
       expect(factory).toBeDefined();
@@ -17,16 +25,20 @@
     }));
 
     it('calls API service', inject(function() {
-      $httpBackend.expectGET('https://api.github.com/repos/benmurden/transpositron/contributors?per_page=30')
+      $httpBackend.expectGET(apiUrl + '30')
         .respond({});
 
       factory.getContributors();
       $httpBackend.flush();
     }));
 
-    afterEach(function() {
-      $httpBackend.verifyNoOutstandingExpectation();
-      $httpBackend.verifyNoOutstandingRequest();
-    });
+    it('handles API error', inject(function() {
+      $httpBackend.whenGET(apiUrl + '30')
+        .respond(500, '');
+
+      factory.getContributors();
+      $httpBackend.flush();
+      expect($log.error.logs.length).toEqual(1);
+    }));
   });
 })();
