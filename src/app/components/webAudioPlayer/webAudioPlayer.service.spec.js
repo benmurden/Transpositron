0df1d1b4e676e8bee395a74769158aa920bf9c6d
Benjamin Murden <ben.murden@gmail.com>
2016-05-26 15:55:37 +0900

    Add more tests for webAudioPlayer

--- src/app/components/webAudioPlayer/webAudioPlayer.service.spec.js
+++ src/app/components/webAudioPlayer/webAudioPlayer.service.spec.js
@@ -17,6 +17,13 @@
     }));
 
     describe('setWaveform', function() {
+      var deferred, $rootScope;
+
+      beforeEach(inject(function($q, _$rootScope_) {
+        deferred = $q.defer();
+        $rootScope = _$rootScope_;
+      }));
+
       it('sets standard oscillator type', inject(function() {
         spyOn(wavetables, 'getWavetable');
         service.setWaveform('square');
@@ -25,8 +32,7 @@
         expect(service.waveform).toEqual('square');
       }));
 
-      it('gets custom waveform from waveform service', inject(function($q, $rootScope) {
-        var deferred = $q.defer();
+      it('gets custom waveform from waveform service', inject(function() {
         deferred.resolve({real: [], imag: []});
 
         spyOn(wavetables, 'getWavetable').and.returnValue(deferred.promise);
@@ -37,6 +43,50 @@
         expect(wavetables.getWavetable).toHaveBeenCalledWith('bass');
         expect(service.waveform).toEqual('bass');
         expect(service.waveforms.bass).toBeDefined();
+      }));
+
+      it('displays error when wavetable retreival fails', inject(function(toastr) {
+        deferred.reject(404);
+
+        spyOn(wavetables, 'getWavetable').and.returnValue(deferred.promise);
+        spyOn(toastr, 'error');
+
+        service.setWaveform('bass');
+        $rootScope.$apply();
+
+        expect(wavetables.getWavetable).toHaveBeenCalledWith('bass');
+        expect(toastr.error).toHaveBeenCalled();
+        expect(service.waveforms.bass).not.toBeDefined();
+      }));
+    });
+
+    describe('buildOscillatorObject', function() {
+      it('builds standard oscillator', inject(function() {
+        var result;
+        service.waveform = 'square';
+        result = service.buildOscillatorObject('C3');
+
+        expect(result).toBeDefined();
+        expect(result.osc).toBeDefined();
+        expect(result.osc.type).toEqual('square');
+      }));
+
+      it('builds wavetable oscillator', inject(function() {
+        var result;
+        service.waveform = 'bass';
+        service.waveforms = {'bass': {}};
+        result = service.buildOscillatorObject('C3');
+
+        expect(result).toBeDefined();
+        expect(result.osc).toBeDefined();
+      }));
+
+      it('warns when wavetable instrument is not ready yet', inject(function(toastr) {
+        service.waveform = 'bass';
+        spyOn(toastr, 'warning');
+        service.buildOscillatorObject('C3');
+
+        expect(toastr.warning).toHaveBeenCalled();
       }));
     });
   });
